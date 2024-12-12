eb35580f48bfbb3d5a4416be89ffcac0638e9c7e
Don Paruyr <pasha172008@mail.ru>
2024-12-13 01:19:06 +0300

    initial

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Button = () => {
+  return (
+    <button {...propos} className={'button ' + props.className}></button>
+  )
+}
+
+export default Button
+
+//rafce
